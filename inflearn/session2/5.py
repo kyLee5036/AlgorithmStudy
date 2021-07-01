4963ba711f618ae514b4146e0a0b8d68e5fc09dd
KeonYoungLeee <ygy5036@gmail.com>
2021-07-02 08:33:51 +0900

    (solve) inflearn 2-5

---
+++ inflearn/session2/5.py
@@ -0,0 +1,19 @@
+import sys
+# sys.stdin=open("input.txt", "rt")
+
+N, M = list(map(int, input().split()))
+cnt = [0]*(N+M+3)
+
+for i in range(1, N+1):
+  for j in range(1, M+1):
+    cnt[i+j]+=1
+    
+max = -214700000
+for i in range(N+M+1):
+  if cnt[i] > max:
+    max = cnt[i]
+    
+for i in range(N+M+1):
+  if cnt[i] == max:
+    print(i, end=' ')
+    
