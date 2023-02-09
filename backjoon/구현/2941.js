76f1764ff81fd4a120e39ef537d3652e4cdb1452
LEEKY <37015435+kyLee5036@users.noreply.github.com>
2023-02-09 23:55:51 +0900

    2941

--- backjoon/구현/2941.js
+++ backjoon/구현/2941.js
@@ -6,14 +6,24 @@ const fileRead = (kinds) => {
 }
 const input = process.platform === 'win32' ? fileRead(TEST_ENVIRONMENT) : fileRead(REAL_ENVIRONMENT);
 
+// ①
 const xAlpahBet = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z='];
-
 function solution() {
     let inputData = input;
     for (let alphabet of xAlpahBet) {
         inputData = inputData.split(alphabet).join("■");
     }
     return inputData.length
+}
+
+// ②
+const xAlpahBetRegex = [/c=/g, /c-/g, /dz=/g, /d-/g, /lj/g, /nj/g, /s=/g, /z=/g];
+function solution() {
+    let inputData = input;
+    for (const x of xAlpahBetRegex) {
+        inputData = inputData.replace(x, '■');
+    }
+    return inputData.length;
 }
 
 console.log(solution())
