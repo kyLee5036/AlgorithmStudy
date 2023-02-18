e387b20fcd2c7a47606388e76da1a31fb7eddece
LEEKY <37015435+kyLee5036@users.noreply.github.com>
2023-02-19 01:48:29 +0900

    1978

--- backjoon/구현/1978.js
+++ backjoon/구현/1978.js
@@ -30,13 +30,13 @@ function isPrime (x) {
 };
 
 function isPrime (x) {
-	if( x == 1 ) return false;
-	for ( let i = 2; i < x; i++ ) {
-		if ( x % i == 0 ) {
+    if( x == 1 ) return false;
+    for ( let i = 2; i < x; i++ ) {
+	if ( x % i == 0 ) {
             return false;
         }
-	}
-	return true;
+    }
+    return true;
 }
 
 solution();
